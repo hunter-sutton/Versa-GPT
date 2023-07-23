998673a7ce49e8517aa9f03e643ddbdc04dd4344
Hunter Sutton <hwsutton43@gmail.com>
2023-07-22 19:18:14 -0700

    Implemented Chat class

--- Chat.py
+++ Chat.py
@@ -1,3 +1,91 @@
 class Chat:
-    def __init__(self, name):
-        self.name = name
+    chat_history = []
+    user_message = ""
+    chat_name = ""
+    model = ""
+    system_prompt = ""
+    temperature = 1
+
+    def __init__(self):
+        self.initChatName()
+        self.initSystemPrompt()
+        self.modelSelect()
+        self.chatHistoryAppend("system", self.system_prompt)
+    
+    def title(self):
+        print("\033c")
+        print(" __   __   ______     ______     ______     ______        ______     ______   ______  \n/\ \ / /  /\  ___\   /\  == \   /\  ___\   /\  __ \      /\  ___\   /\  == \ /\__  _\ \n\ \ \\'/   \ \  __\   \ \  __<   \ \___  \  \ \  __ \     \ \ \__ \  \ \  _-/ \/_/\ \/ \n \ \__|    \ \_____\  \ \_\ \_\  \/\_____\  \ \_\ \_\     \ \_____\  \ \_\      \ \_\ \n  \/_/      \/_____/   \/_/ /_/   \/_____/   \/_/\/_/      \/_____/   \/_/       \/_/ \n                                                                                      ")
+
+    def chatHistoryAppend(self, role, content):
+        self.chat_history.append({
+            "role": role,
+            "content": content
+        })
+
+    def printChatHistory(self):
+        for message in self.chat_history:
+            if message["role"] == "user":
+                print("\033[30;47mYou\033[0m")
+            else:
+                print("\033[30;47m" + message["role"] + "\033[0m")
+            print(message["content"])
+            print()
+
+    def chatInfo(self):
+        print("===", self.chat_name, "===")
+        print("=== Model: ", self.model, "===")
+        print("=== Prompt: ", self.system_prompt, "===")
+    
+    def modelSelect(self):
+        # User selects the model to use
+        print("Please select a model:")
+        print("1. gpt-4")
+        print("2. gpt-4-32k")
+        print("3. gpt-3.5-turbo")
+        print("4. gpt-3.5-turbo-16k")
+
+        model_choice = input("Your choice: ")
+
+        if model_choice == '1':
+            self.model = 'gpt-4'
+        elif model_choice == '2':
+            self.model = 'gpt-4-32k'
+        elif model_choice == '3':
+            self.model = 'gpt-3.5-turbo'
+        elif model_choice == '4':
+            self.model = 'gpt-3.5-turbo-16k'
+        else:
+            self.model = 'gpt-3.5-turbo'
+
+    def initChatName(self):
+        user_input = input("Name of Chatbot: ")
+        if user_input == '':
+            self.chat_name = "ChatGPT"
+        else:
+            self.chat_name = user_input
+    
+    def initSystemPrompt(self):
+        user_input = input("Your prompt: ")
+        if user_input == '':
+            self.system_prompt = "You are a helpful assistant."
+        else:
+            self.system_prompt = user_input
+    
+    def getUserInput(self):
+        user_input = []
+        print("\033[30;47mYou\033[0m")
+        while True:
+            line = input("> ")
+            if line == '':
+                break
+            else:
+                user_input.append(line)
+        user_input = '\n'.join(user_input)
+        self.user_message = user_input
+    
+    def printCommands(self):
+        print("quit - quit the program")
+        print("clear - clear the chat history")
+        print("pdf - read a PDF file")
+        print("history - print the chat history")
+        print("help - print this help message")
--- main.py
+++ main.py
@@ -3,160 +3,65 @@ import openai
 import datetime
 import tokenizer
 
+from Chat import Chat
 import plugins.pdfReader.readPDF as readPDF
 
 openai.api_key = dotenv.get_key(dotenv.find_dotenv(), "OPENAI_API_KEY")
 
-def chatInfo(chat_name, model, prompt):
-    print("\033c")
-    print("===", chat_name, "===")
-    print("=== Model: ", model, "===")
-    print("=== Prompt: ", prompt, "===")
-
-def modelSelect():
-    # User selects the model to use
-    print("Please select a model:")
-    print("1. gpt-4")
-    print("2. gpt-4-32k")
-    print("3. gpt-3.5-turbo")
-    print("4. gpt-3.5-turbo-16k")
-
-    model_choice = input("Your choice: ")
-
-    if model_choice == '1':
-        model = 'gpt-4'
-    elif model_choice == '2':
-        model = 'gpt-4-32k'
-    elif model_choice == '3':
-        model = 'gpt-3.5-turbo'
-    elif model_choice == '4':
-        model = 'gpt-3.5-turbo-16k'
-    else:
-        model = 'gpt-3.5-turbo'
-
-    return model
-
-def chatHistoryInit(prompt):
-    chat_history = [
-        {
-            "role": "system",
-            "content": prompt
-        }
-    ]
-
-    return chat_history
-
-def chatHistoryAppend(chat_history, userOrSystem, content):
-    role = "user" if userOrSystem == "user" else "assistant"
-
-    chat_history.append({
-        "role": role,
-        "content": content
-    })
-
-def printChatHistory(chat_history):
-    for message in chat_history:
-        if message["role"] == "user":
-            print("\033[30;47mYou\033[0m")
-        else:
-            print("\033[30;47mAssistant\033[0m")
-        print(message["content"])
-        print()
-
-# Clear the terminal
 print("\033c")
 
-# User selects the model to use
-model = modelSelect()
+# Create a new chat instance
+chat = Chat()
 
-chat_name = input("Name of Chatbot: ")
-if chat_name == '':
-    chat_name = "ChatGPT"
-
-prompt = input("Your prompt: ")
-if prompt == '':
-    prompt = "You are a helpful assistant."
-
-# Store the prompt in a file for if the user wants to use it again
-# Check if the file already exists, if it does, append to it
+# Save the system prompt to a file
 try:
     with open("prompts.txt", "a") as prompts_file:
-        prompts_file.write("\n\n" + chat_name + " - " + str(datetime.datetime.now()) + "\n\n" + prompt)
+        prompts_file.write("\n\n" + chat.chat_name + " - " + str(datetime.datetime.now()) + "\n\n" + chat.system_prompt)
 except:
     with open("prompts.txt", "w") as prompts_file:
-        prompts_file.write("\n\n" + chat_name + " - " + str(datetime.datetime.now()) + "\n\n" + prompt)
+        prompts_file.write("\n\n" + chat.chat_name + " - " + str(datetime.datetime.now()) + "\n\n" + chat.system_prompt)
 
-# Initialize the chat history
-chat_history = chatHistoryInit(prompt)
-
-chatInfo(chat_name, model, prompt)
+chat.title()
+chat.chatInfo()
+print()
 
 # Create a while loop that allows the user to keep chatting
-print()
 while True:
-    # Holds each line of the user's input for a single message
-    user_input = []
+    chat.getUserInput()
 
-    # Get the user's input. If the user enters a blank line, send the message
-    # Otherwise, keep appending to the user_input list
-    print("\033[30;47mYou\033[0m")
-    while True:
-        line = input("> ")
-        if line == '':
-            break
-        else:
-            user_input.append(line)
-
-    # Join the user_input list into a single string where each line is separated by a newline
-    user_input = '\n'.join(user_input)
-    
-    if user_input.lower() == 'quit':
+    if chat.user_message.lower() == 'quit':
         break
-
-    elif user_input.lower() == 'clear':
-        chat_history = chatHistoryInit(prompt)
-        chatInfo(chat_name, model, prompt)
+    elif chat.user_message.lower() == 'clear':
+        chat.chat_history = []
+        chat.chatHistoryAppend("system", chat.system_prompt)
+        chat.chatInfo()
         continue
-
-    elif user_input.lower() == 'pdf':
+    elif chat.user_message.lower() == 'pdf':
         pdf_name = input("PDF Name: ")
         pdf_text = readPDF.readTextFromFile(pdf_name)
-        pdf_text = readPDF.addPrefixSuffix("", "", pdf_text, pdf_name)
-        tokens = tokenizer.calculateTokens(pdf_text, model)
-        print("Number of tokens: " + str(len(tokens)))
-        print("Is this acceptable? (y/n)")
-        user_input = input("> ")
-        if user_input.lower() == 'y':
-            chatHistoryAppend(chat_history, "user", pdf_text)
+        tokens = tokenizer.calculateTokens(pdf_text, chat.model)
+        if tokenizer.verifyTokens(tokens):
+            chat.chatHistoryAppend("user", pdf_text)
         else:
             continue
-        chatHistoryAppend(chat_history, "user", pdf_text)
-
-    elif user_input.lower() == 'history':
-        printChatHistory(chat_history)
+    elif chat.user_message.lower() == 'history':
+        chat.printChatHistory()
         continue
-
-    elif user_input.lower() == 'help':
-        print("quit - quit the program")
-        print("clear - clear the chat history")
-        print("pdf - read a PDF file")
-        print("history - print the chat history")
-        print("help - print this help message")
+    elif chat.user_message.lower() == 'help':
+        chat.printCommands()
         continue
-
-    elif user_input.lower() == '':
-        print("Please enter a message.")
+    elif chat.user_message.lower() == '':
+        print("Please enter a message or `help` to see other commands.")
         continue
-
     else:
-        chatHistoryAppend(chat_history, "user", user_input)
+        chat.chatHistoryAppend("user", chat.user_message)
 
     # Request a response from the selected model
     try:
         response = openai.ChatCompletion.create(
-            model=model,
-            messages=chat_history, # Pass the updated chat history
-            temperature=1,
+            model=chat.model,
+            messages=chat.chat_history, # Pass the updated chat history
+            temperature=chat.temperature,
             max_tokens=2048,
             top_p=1,
             frequency_penalty=0,
@@ -177,9 +82,9 @@ while True:
 
     # Print the model's response
     print()
-    print("\033[30;47m" + "==" + chat_name + "==" + "\033[0m")
+    print("\033[30;47m" + "==" + chat.chat_name + "==" + "\033[0m")
     print(response["choices"][0]["message"]["content"])
     print()
 
     # Append the model's response to the chat history
-    chatHistoryAppend(chat_history, "system", response["choices"][0]["message"]["content"])
+    chat.chatHistoryAppend("assistant", response["choices"][0]["message"]["content"])
